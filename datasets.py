d209f5337ec3c43529129dc3ab9a1c64f57413b6
JisooHan1 <jshan3328@naver.com>
2024-08-06 02:41:12 +0900

    <code organize>
    - Remove whitespace
    main.py
    - Reorder parameters
    - Rename execution options
    
    datasets.py
    - Different test transform
    - Parameterize load_dataset.input_channels
    - Add STL10

--- datasets.py
+++ datasets.py
@@ -2,29 +2,45 @@ import torchvision
 import torchvision.transforms as transforms
 
 def load_dataset(name):
-    transform_pars = [transforms.ToTensor(),
-                      transforms.RandomResizedCrop(32, scale=(0.5,1))]
+
+    if name in ["CIFAR10","STL10"]:
+        load_dataset.input_channels = 3
+
+    elif name in ["MNIST"]:
+        load_dataset.input_channels = 1
+
+    mean = (0.5,)*load_dataset.input_channels
+    std = mean
+
+    train_transform_pars = [
+            transforms.ToTensor(),
+            transforms.RandomResizedCrop(32),
+            transforms.Normalize(mean, std)]
+
+    test_transform_pars = [
+        transforms.ToTensor(),
+        transforms.Resize(32),
+        transforms.Normalize(mean, std)]
 
     if name == "CIFAR10":
-        input_channels = 3
-        transform_pars.extend([
-            transforms.RandomHorizontalFlip(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        train_transform_pars.extend([transforms.RandomHorizontalFlip()])
         dataset = torchvision.datasets.CIFAR10
-        
+
+    elif name == "STL10":
+        train_transform_pars.extend([transforms.RandomHorizontalFlip()])
+        dataset = torchvision.datasets.STL10
+
     elif name == "MNIST":
-        input_channels = 1
-        transform_pars.extend([
-            transforms.Normalize((0.5), (0.5))])
         dataset = torchvision.datasets.MNIST
-        
+
     else:
         raise ValueError("Invalid dataset name")
-    
-    transform = transforms.Compose(transform_pars)
-    trainset = dataset(root='./data', train=True, download=True, transform=transform)
-    testset = dataset(root='./data', train=False, download=True, transform=transform)
-    
-    return trainset, testset, input_channels
-    
-    
+
+    train_transform = transforms.Compose(train_transform_pars)
+    test_transform = transforms.Compose(test_transform_pars)
+
+    trainset = dataset(root='./data', train=True, download=True, transform=train_transform)
+    testset = dataset(root='./data', train=False, download=True, transform=test_transform)
+
+    return trainset, testset
+
--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from models import load_model
 import argparse
 from torch.utils.tensorboard import SummaryWriter
 
-def train(net, num_epoch, trainloader, optimizer, criterion, writer):
+def train(net, trainloader, criterion, optimizer, num_epoch, writer):
     for epoch in range(num_epoch):
         running_loss = 0.0
-        
+
         for i, data in enumerate(trainloader, 0):
             inputs, labels = data
-            
+
             optimizer.zero_grad()
 
             # forward + backward + optimize
@@ -21,18 +21,18 @@ def train(net, num_epoch, trainloader, optimizer, criterion, writer):
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-            
+
             # print statistics
             running_loss += loss.item()
             if i % 2000 == 1999:    # print every 2000 mini-batches
                 print('[%d, %5d] loss: %.3f' %(epoch + 1, i + 1, running_loss / 2000))
                 writer.add_scalar('Loss/train', running_loss / 2000, epoch * len(trainloader) + i) # global index for loss
                 running_loss = 0.0
-                
+
         writer.add_scalar('Loss/train_epoch', running_loss, epoch)
     print('Finished Training')
-    
-def test(testloader, net, criterion, num_epoch, writer ):
+
+def test(net, testloader, criterion, num_epoch, writer):
     net.eval()
     correct = 0
     total = 0
@@ -41,10 +41,10 @@ def test(testloader, net, criterion, num_epoch, writer ):
         for data in testloader:
             images, labels = data
             outputs = net(images)
-            
+
             loss = criterion(outputs, labels)
             test_loss += loss.item()
-            
+
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
@@ -57,15 +57,15 @@ def test(testloader, net, criterion, num_epoch, writer ):
 def main():
     # Parsing command 정의
     parser = argparse.ArgumentParser(description="Executes deep learning using CCN")
-    parser.add_argument("-m", "--model", type=str, required=True, help="type of model: LeNet-5")
-    parser.add_argument("-d", "--dataset", type=str, required=True, help="type of dataset: CIFAR-10, MNIST")
-    parser.add_argument("-e", "--num_epochs", type=int, required=True, help="number of epochs")
-    parser.add_argument("-b", "--batch_size", type=int, required=True, help="batch size")
+    parser.add_argument("-md", "--model", type=str, required=True, help="type of model: LeNet-5")
+    parser.add_argument("-ds", "--dataset", type=str, required=True, help="type of dataset: CIFAR-10, MNIST")
+    parser.add_argument("-ep", "--num_epochs", type=int, required=True, help="number of epochs")
+    parser.add_argument("-bs", "--batch_size", type=int, required=True, help="batch size")
     args = parser.parse_args()
 
     # Parsing 변수 정의
-    trainset, testset, input_channels = load_dataset(args.dataset)
-    net = load_model(args.model, input_channels)
+    trainset, testset = load_dataset(args.dataset)
+    net = load_model(args.model, load_dataset.input_channels)
     num_epoch = args.num_epochs
     batch_size = args.batch_size
 
@@ -73,11 +73,11 @@ def main():
     testloader = torch.utils.data.DataLoader(testset, batch_size, shuffle=False, num_workers=2)
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-    
+    optimizer = optim.Adam(net.parameters(), lr=1e-3)
+
     writer = SummaryWriter(log_dir='logs')
-    train(net, num_epoch, trainloader, optimizer, criterion, writer)
-    test(testloader, net, criterion, num_epoch, writer)
+    train(net, trainloader, criterion, optimizer, num_epoch, writer)
+    test(net, testloader, criterion, num_epoch, writer)
 
 if __name__ == "__main__":
     main()
