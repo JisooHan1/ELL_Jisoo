1e3d9b4c420490713694d65e348b2fe8e67c1c34
Jisoo <jshan3328@naver.com>
2024-09-25 23:02:05 +0900

    fractalnet reduced

--- src/main.py
+++ src/main.py
@@ -127,11 +127,14 @@ def main():
         optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9)
         milestones = [epoch*0.5, epoch*0.75]
     elif args.model == "FractalNet": # batch size: 64, epoch: 100
-        lr = 0.02
-        optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9)
-        # milestones = [epoch*0.5, epoch*0.75]
-        milestones = [epoch // 2**i for i in range(1, int(math.log2(epoch)) + 1)]
-        milestones.reverse()
+        lr = 0.001
+        optimizer = optim.Adam(net.parameters(), lr=lr)
+        milestones = []
+        # lr = 0.02
+        # optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9)
+        # # milestones = [epoch*0.5, epoch*0.75]
+        # milestones = [epoch // 2**i for i in range(1, int(math.log2(epoch)) + 1)]
+        # milestones.reverse()
     elif args.model == "ViT": # batch size: 64, epoch: 100
         lr = 0.001
         optimizer = optim.Adam(net.parameters(), lr=lr)
