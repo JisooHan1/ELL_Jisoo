d0b5216dfb24cdfb093d1d652d886c7a6d7bba1b
Jisoo <jshan3328@naver.com>
2024-09-30 00:58:00 +0900

    revised fractalnet

--- src/main.py
+++ src/main.py
@@ -128,10 +128,11 @@ def main():
         milestones = [epoch*0.5, epoch*0.75]
     elif args.model == "FractalNet": # batch size: 64, epoch: 100
         lr = 0.001
-        optimizer = optim.Adam(net.parameters(), lr=lr)
-        milestones = [epoch*0.25, epoch*0.5, epoch*0.75]
+        optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9)
+        milestones = [epoch // 2**i for i in range(1, int(math.log2(epoch)) + 1)]
+        milestones.reverse()
         # lr = 0.02
-        # optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9)
+        # optimizer = optim.Adam(net.parameters(), lr=lr)
         # # milestones = [epoch*0.5, epoch*0.75]
         # milestones = [epoch // 2**i for i in range(1, int(math.log2(epoch)) + 1)]
         # milestones.reverse()
