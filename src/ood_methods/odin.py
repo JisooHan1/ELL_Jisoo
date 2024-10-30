6ea0cef21680ed8cbfbdb6bd54d21a14cd10a1d5
Jisoo <jshan3328@naver.com>
2024-10-31 01:25:22 +0900

    ood odin revised

--- src/ood_methods/odin.py
+++ src/ood_methods/odin.py
@@ -4,6 +4,8 @@ import torch.nn.functional as F
 def odin_score(input_data, model, temperature=1000, epsilon=0.001):
     input_data.requires_grad = True
 
+    model.eval()
+
     outputs_1 =  model(input_data) / temperature
     softmax_scores = F.softmax(outputs_1, dim=1)
     max_score_1, pred_class = torch.max(softmax_scores, dim=1)
@@ -17,8 +19,9 @@ def odin_score(input_data, model, temperature=1000, epsilon=0.001):
     processed_input_data = input_data - epsilon * torch.sign(gradient)
     processed_input_data = processed_input_data.detach()
 
-    outputs_2 = model(processed_input_data) / temperature
-    calibrated_softmax_scores = F.softmax(outputs_2, dim=1)
-    max_score_2, _ = torch.max(calibrated_softmax_scores, dim=1)
+    with torch.no_grad():
+        outputs_2 = model(processed_input_data) / temperature
+        calibrated_softmax_scores = F.softmax(outputs_2, dim=1)
+        max_score_2, _ = torch.max(calibrated_softmax_scores, dim=1)
 
     return max_score_2
