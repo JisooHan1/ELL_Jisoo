42e06d75f75c4a87f1a26889b8f01b96abb523df
Jisoo <jshan3328@naver.com>
2024-10-31 00:50:04 +0900

    ood odin added

--- src/ood_methods/odin.py
+++ src/ood_methods/odin.py
@@ -2,18 +2,21 @@ import torch
 import torch.nn.functional as F
 
 def odin_score(input_data, model, temperature=1000, epsilon=0.001):
-
     input_data.requires_grad = True
 
     outputs_1 =  model(input_data) / temperature
-    negative_log_softmax_scores = -F.log_softmax(outputs_1, dim=1)  # (batch_size, num_class)
-    negative_log_softmax_scores.backward(torch.ones_like(negative_log_softmax_scores))
+    softmax_scores = F.softmax(outputs_1, dim=1)
+    max_score_1, pred_class = torch.max(softmax_scores, dim=1)
+
+    log_softmax_scores = F.log_softmax(outputs_1, dim=1)
+    negative_log_softmax = -log_softmax_scores[range(len(pred_class)), pred_class]
+    negative_log_softmax.sum().backward()
 
     gradient = input_data.grad
     processed_input_data = input_data - epsilon * torch.sign(gradient)
 
     outputs_2 = model(processed_input_data) / temperature
     calibrated_softmax_scores = F.softmax(outputs_2, dim=1)
-    max_score, _ = torch.max(calibrated_softmax_scores, dim=1)
+    max_score_2, _ = torch.max(calibrated_softmax_scores, dim=1)
 
-    return max_score
+    return max_score_2
