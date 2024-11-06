d3f3a504d69b32baf29463bd295b33313fb11669
Jisoo <jshan3328@naver.com>
2024-11-07 03:48:41 +0900

    mds

--- src/ood_detection.py
+++ src/ood_detection.py
@@ -8,6 +8,8 @@ import argparse
 from torcheval.metrics import BinaryAUROC, BinaryAUPRC
 from ood_methods import get_ood_methods
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 # Definition
 id_layer_outputs = {}
 ood_layer_outputs = {}
@@ -20,8 +22,8 @@ avg_pool = nn.AdaptiveAvgPool2d((1, 1))
 
 # Model loading function
 def load_model(model_path):
-    model = DenseNet(3)
-    state_dict = torch.load(model_path, map_location='cpu')
+    model = DenseNet(3).to(device)
+    state_dict = torch.load(model_path, map_location=device)
     model.load_state_dict(state_dict, strict=True)
     model.eval()
     for param in model.parameters():
@@ -55,6 +57,7 @@ if __name__ == '__main__':
 
     # Compute class means and covariances
     for inputs, labels in id_train_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
         model(inputs)
         for layer_index, (layer_name, output) in enumerate(id_layer_outputs.items()):
             for i, label in enumerate(labels):
@@ -66,7 +69,7 @@ if __name__ == '__main__':
         N = sum(len(class_features[layer][cls]) for cls in range(num_classes))
 
         for cls in range(num_classes):
-            class_data = torch.cat(class_features[layer][cls], dim=0)
+            class_data = torch.cat(class_features[layer][cls], dim=0).to(device)
             cls_means[layer][cls] = torch.mean(class_data, dim=0)
 
             deviations = class_data - cls_means[layer][cls]
@@ -88,8 +91,10 @@ if __name__ == '__main__':
 
         model(test_sample)
 
+        test_sample = test_sample.to(device)
+
         for layer in range(num_layers):
-            ood_layer_output = ood_layer_outputs[f"layer_{layer}"]
+            ood_layer_output = ood_layer_outputs[f"layer_{layer}"].to(device)
 
             min_distance = 0
             closest_class = None
@@ -115,7 +120,7 @@ if __name__ == '__main__':
 # Example usage:
 alpha_weights = [1.0] * num_layers  # Define your layer-specific weights
 epsilon = 0.01  # Small noise level
-test_sample = torch.randn(3, 32, 32)  # Example test sample in CIFAR-10 format (3 channels, 32x32)
+test_sample = torch.randn(3, 32, 32).to(device)  # Example test sample in CIFAR-10 format (3 channels, 32x32)
 confidence_score = mds_score(model, test_sample, epsilon, cls_means, cls_covariances, num_layers, alpha_weights)
 print("Confidence Score:", confidence_score)
 
