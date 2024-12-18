be85b5b8d86e48c1bfa5582e40bc12c6f565f7af
Jisoo <jshan3328@naver.com>
2024-12-19 00:04:56 +0900

    updated ood

--- src/ood_detection.py
+++ src/ood_detection.py
@@ -69,13 +69,13 @@ def run_ood_detection(args):
     ood_method = get_ood_methods(args.method, model=model)
     ood_method.apply_method(id_loader)
 
-    for data in id_loader:
-        batch_id_scores = ood_method.ood_score(data.to(device))
+    for inputs, _ in id_loader:
+        batch_id_scores = ood_method.ood_score(inputs.to(device))
         id_scores.append(batch_id_scores)
     id_scores = torch.cat(id_scores)
 
-    for data in ood_loader:
-        batch_ood_scores = ood_method.ood_score(data.to(device))
+    for inputs, _ in ood_loader:
+        batch_ood_scores = ood_method.ood_score(inputs.to(device))
         ood_scores.append(batch_ood_scores)
     ood_scores = torch.cat(ood_scores)
 
