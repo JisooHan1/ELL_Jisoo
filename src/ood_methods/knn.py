53912330916f4c858b85bcb791f6a11979cb056a
Jisoo <jshan3328@naver.com>
2024-12-19 00:13:48 +0900

    updated ood

--- src/ood_methods/knn.py
+++ src/ood_methods/knn.py
@@ -7,16 +7,16 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class KNN(BaseOOD):
     def __init__(self, model, k=50):
         super().__init__(model)
-        self.id_features = None
+        self.id_features = torch.tensor([], device=device)
         self.k = k
 
     # get normalized id features
     def get_features(self, id_loader):
         for inputs, _ in id_loader:
-            inputs = inputs.to(device)
             self.model(inputs)
-        self.id_features = F.normalize(self.penultimate_layer.flatten(1), p=2, dim=1)  # (batch x 512)
-        return self.id_features  # (batch x 512)
+            self.id_features = torch.cat([self.id_features, self.penultimate_layer.flatten(1)])
+        self.id_features = F.normalize(self.id_features, p=2, dim=1)  # (num_samples x channel)
+        return self.id_features
 
     # apply method
     def apply_method(self, id_loader):
@@ -27,10 +27,10 @@ class KNN(BaseOOD):
         inputs = inputs.to(device)
         self.model(inputs)
 
-        features = self.penultimate_layer.flatten(1)  # (batch x 512)
+        features = self.penultimate_layer.flatten(1)  # (batch x channel)
         l2_features = F.normalize(features, p=2, dim=1)
 
-        distances = torch.cdist(l2_features, self.id_features)  # (batch x batch)
+        distances = torch.cdist(l2_features, self.id_features)  # (batch x num_samples)
         distances, _ = torch.sort(distances, dim=1, descending=False)
 
         kth_distance = distances[self.k - 1]
