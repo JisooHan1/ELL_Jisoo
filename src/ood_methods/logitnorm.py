7ae2c8bdea9889f9802018a9fc402639eecf0707
Jisoo <jshan3328@naver.com>
2025-01-07 13:59:24 +0900

    feature: mds_2.py-added

--- src/ood_methods/logitnorm.py
+++ src/ood_methods/logitnorm.py
@@ -4,22 +4,6 @@ import torch.nn.functional as F
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-'''
-training method
-
-1. output of the model: logit_vector = f => 클래스 차원의 벡터
-2. f_i = ||f||*f_i: 클래스-i 차원의 값
-3. decompose the logit: logit_vector = ||f|| * f/||f||
-4. f_hat = f/||f||
-
-<code-flow>
-1. load the model
-2. load the data
-3. get the output of the model
-4. normalize the output
-5. get the score
-'''
-
 class LogitNormLoss(nn.Module):
     def __init__(self, tau=0.04):
         super(LogitNormLoss, self).__init__()
