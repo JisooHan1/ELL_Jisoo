3c081ef6d2c86bde869f8f99596742c056c4270c
Jisoo <jshan3328@naver.com>
2025-01-14 01:49:54 +0900

    feature: datasets.py - TinyImageNet200 added

--- src/datasets.py
+++ src/datasets.py
@@ -5,6 +5,37 @@ from torch.utils.data import Subset
 import torchvision
 import torchvision.transforms as transforms
 
+class TinyImageNet200(torchvision.datasets.ImageFolder):
+    def __init__(self, root, split='train', transform=None):  # root = ./datasets/tiny-imagenet-200
+        self.samples = []
+        self.targets = []
+
+        # split = train
+        if split == 'train':
+            root = os.path.join(root, 'train')  # root = ./datasets/tiny-imagenet-200/train
+
+        # split = val
+        else:
+            root = os.path.join(root, 'val')  # root = ./datasets/tiny-imagenet-200/val
+            val_images_path = os.path.join(root, 'images')  # ./datasets/tiny-imagenet-200/val/images
+            val_labels_path = os.path.join(root, 'val_annotations.txt')  # ./datasets/tiny-imagenet-200/val/val_annotations.txt
+
+            # get class index
+            train_root = os.path.join(os.path.dirname(root), 'train')  # train_root = ./datasets/tiny-imagenet-200/train
+            self.cls_idx = {cls: idx for idx, cls in enumerate(sorted(os.listdir(train_root)))}  # {cls: idx}
+
+            # get image and label
+            with open(val_labels_path, 'r') as f:
+                for line in f:
+                    image_name, label, *_ = line.strip().split('\t')  # [image_name, label, *]
+                    image_path = os.path.join(val_images_path, image_name)
+                    cls_idx = self.cls_idx[label]
+                    self.samples.append((image_path, cls_idx))
+                    self.targets.append(cls_idx)
+
+        # set transform
+        super(TinyImageNet200, self).__init__(root=root, transform=transform)
+
 dataset_config = {
 
         "TinyImageNet": {
@@ -12,11 +43,11 @@ dataset_config = {
             # 200 class
             # 100,000 training images
             # 10,000 test images
-            "dataset": torchvision.datasets.ImageFolder,
+            "dataset": TinyImageNet200,
             "image size": 32,
             "input channel": 3,
-            "train option": {"root": "./datasets/tiny-imagenet-200/train"},
-            "test option": {"root": "./datasets/tiny-imagenet-200/val/images"}
+            "train option": {"split": "train"},
+            "test option": {"split": "val"}
         },
 
 
@@ -112,7 +143,7 @@ def load_dataset(name):
     test_transform_pars = transform_pars + [
         transforms.Resize(config["image size"])]
 
-    if name in ["CIFAR10", "STL10", "SVHN", "CIFAR100", "LSUN"]:
+    if name in ["CIFAR10", "STL10", "SVHN", "CIFAR100", "LSUN", "TinyImageNet200"]:
         train_transform_pars.append(transforms.RandomHorizontalFlip())
 
     # transformation pipeline
@@ -120,11 +151,15 @@ def load_dataset(name):
     test_transform = transforms.Compose(test_transform_pars)
 
     # load datasets with transformations
-    trainset = config["dataset"](root='./datasets', **config["train option"], download=True, transform=train_transform)
-    testset = config["dataset"](root='./datasets', **config["test option"], download=True, transform=test_transform)
+    if name == "TinyImageNet200":
+        trainset = config["dataset"](root='./datasets/tiny-imagenet-200/train', **config["train option"], download=True, transform=train_transform)
+        testset = config["dataset"](root='./datasets/tiny-imagenet-200/val/images', **config["test option"], download=True, transform=test_transform)
+    else:
+        trainset = config["dataset"](root='./datasets', **config["train option"], download=True, transform=train_transform)
+        testset = config["dataset"](root='./datasets', **config["test option"], download=True, transform=test_transform)
 
-    # subsampling TinyImageNet
-    if name == "TinyImageNet":
+    # subsampling TinyImageNet200
+    if name == "TinyImageNet200":
         train_indices = random.sample(range(len(trainset)), 50000)
         test_indices = random.sample(range(len(testset)), 10000)
         trainset = Subset(trainset, indices=train_indices)
