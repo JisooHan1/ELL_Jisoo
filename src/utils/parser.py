5618bdc70f3a465334b9d61f09337c7f8d31d18e
Jisoo <jshan3328@naver.com>
2025-01-14 04:15:45 +0900

    feature: datasets.py - revised, minor revisions for various files

--- src/utils/parser.py
+++ src/utils/parser.py
@@ -5,16 +5,16 @@ def parse_args():
 
     # for main.py/ood_test.py/ood_train.py
     parser.add_argument("--model", type=str, required=True, help="model")
-    parser.add_argument("--dataset", type=str, required=True, help="dataset")
+    parser.add_argument("--dataset", type=str, required=False, help="dataset")
     parser.add_argument("--augment", type=str, required=False, help="augment data")  # For no augmentation in trainset
-    parser.add_argument("--num_epochs", type=int, required=True, help="number of epochs")
+    parser.add_argument("--epoch", type=int, required=False, help="number of epochs")
     parser.add_argument("--batch_size", type=int, required=True, help="batch size")
 
     # for ood_test.py/ood_train.py
     parser.add_argument("--path", type=str, required=False, help="path to the trained model")  # For ood testing
-    parser.add_argument("--id_dataset", type=str, required=True, help="id dataset")
+    parser.add_argument("--id_dataset", type=str, required=False, help="id dataset")
     parser.add_argument("--oe_dataset", type=str, required=False, help="oe dataset")  # For ood oe based training
-    parser.add_argument("--ood_dataset", type=str, required=True, help="ood dataset")
-    parser.add_argument("--method", type=str, help="ood method")
+    parser.add_argument("--ood_dataset", type=str, required=False, help="ood dataset")
+    parser.add_argument("--method", type=str, required=False, help="ood method")
 
     return parser.parse_args()
