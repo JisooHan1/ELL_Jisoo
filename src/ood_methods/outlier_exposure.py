6480d6bfe5bbbe1c5ca968c616451e086a79c169
Jisoo <jshan3328@naver.com>
2025-01-19 23:16:47 +0900

    oe

--- src/ood_methods/outlier_exposure.py
+++ src/ood_methods/outlier_exposure.py
@@ -7,30 +7,30 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # OutlierExposureLoss
 class OutlierExposureLoss(nn.Module):
-    def __init__(self, alpha=0.5):
+    def __init__(self, lamda=0.5):
         super(OutlierExposureLoss, self).__init__()
-        self.alpha = alpha  # 0.5
+        self.lamda = lamda  # Weight for OE loss
 
     def forward(self, id_outputs, id_labels, oe_outputs):
         # get output(logits) of the model
-        id_outputs, id_labels, oe_outputs = id_outputs.to(device), id_labels.to(device), oe_outputs.to(device)  # (batch x class)
+        id_outputs, id_labels, oe_outputs = id_outputs.to(device), id_labels.to(device), oe_outputs.to(device)
 
         # ID loss
-        ID_loss = F.cross_entropy(id_outputs, id_labels)
+        id_loss = F.cross_entropy(id_outputs, id_labels)
 
         # OE loss: cross entropy with uniform distribution
-        oe_softmax = F.softmax(oe_outputs, dim=1)  # (batch x class)
-        uniform_dist = torch.ones_like(oe_softmax) / oe_softmax.size(1)  # (batch x class)
-        OE_loss = (-torch.log(oe_softmax) * uniform_dist).mean()  # a value
+        oe_mean = oe_outputs.mean(1)  # Mean of logits across classes
+        oe_logsumexp = torch.logsumexp(oe_outputs, dim=1)  # Logsumexp for logits
+        oe_loss = 0.5 * -(oe_mean - oe_logsumexp).mean()  # Loss calculation as per the paper
 
         # total loss
-        total_loss = ID_loss + self.alpha * OE_loss
+        total_loss = id_loss + self.lamda * oe_loss
         return total_loss
 
 # Outlier Exposure training config
 oe_config = {
     "criterion": OutlierExposureLoss,
-    "lr": 0.001,
+    "lr": 0.1,
     "epochs": 10,
     "weight_decay": 5e-4,
     "momentum": 0.9,
