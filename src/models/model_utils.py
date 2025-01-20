311ac12a9d94cc9bd4548a5d7158c7be889ee532
Jisoo <jshan3328@naver.com>
2025-01-20 20:16:23 +0900

    react revision

--- src/models/model_utils.py
+++ src/models/model_utils.py
@@ -12,8 +12,8 @@ from .mlpmixer import MLPMixer
 from .convmixer import ConvMixer
 
 model_path = {
-    "ResNet": {"base": "logs/ResNet/trained_model/ResNet_CIFAR10_True_20250118_1748.pth",
-               "base2": "logs/ResNet/trained_model/ResNet_CIFAR10_True_20250120_0534.pth",
+    "ResNet": {"pre_act": "logs/ResNet/trained_model/ResNet_CIFAR10_True_20250118_1748.pth",
+               "post_act": "logs/ResNet/trained_model/ResNet_CIFAR10_True_20250120_1114.pth",
                "cifar10_no_augment": "logs/ResNet/trained_model/ResNet_CIFAR10_False_20250113_2029.pth",
                "imported": None,
                "logitnorm_cifar10": "logs/ResNet/trained_model/ood_logitnorm_CIFAR10.pth",
--- src/models/resnet.py
+++ src/models/resnet.py
@@ -1,3 +1,78 @@
+# # resnet-34 post-activation
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+
+# class ResBlock(nn.Module):
+#     def __init__(self, in_channel, out_channel, stride):
+#         super(ResBlock, self).__init__()
+
+#         self.conv1 = nn.Conv2d(in_channel, out_channel, kernel_size=3, stride=stride, padding=1)
+#         self.bn1 = nn.BatchNorm2d(out_channel)  # BatchNorm after conv1
+#         self.conv2 = nn.Conv2d(out_channel, out_channel, kernel_size=3, stride=1, padding=1)
+#         self.bn2 = nn.BatchNorm2d(out_channel)  # BatchNorm after conv2
+
+#         self.shortcut = nn.Sequential()
+#         if stride != 1 or in_channel != out_channel:
+#             self.shortcut = nn.Sequential(
+#                 nn.Conv2d(in_channel, out_channel, kernel_size=1, stride=stride, bias=False),
+#                 nn.BatchNorm2d(out_channel))  # Shortcut connection
+
+#     def forward(self, x):
+#         identity = self.shortcut(x)
+#         x = self.conv1(x)  # Convolution first
+#         x = self.bn1(x)   # BatchNorm after Conv
+#         x = F.relu(x)     # ReLU last
+
+#         x = self.conv2(x)  # Second Convolution
+#         x = self.bn2(x)    # BatchNorm after Conv
+#         x += identity      # Add shortcut
+#         x = F.relu(x)      # ReLU applied last
+#         return x
+
+# class ResNet(nn.Module):
+#     def __init__(self, input_channels, num_classes=10):
+#         super(ResNet, self).__init__()
+
+#         self.conv1 = nn.Conv2d(input_channels, 64, 3, stride=1, padding=1)  # First Conv layer
+#         self.bn1 = nn.BatchNorm2d(64)  # BatchNorm for initial Conv
+#         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+#         # Define ResNet-34 layer configuration
+#         self.bundle1 = self.repeat_block(64, 64, 3, 1)
+#         self.bundle2 = self.repeat_block(64, 128, 4, 2)
+#         self.bundle3 = self.repeat_block(128, 256, 6, 2)
+#         self.bundle4 = self.repeat_block(256, 512, 3, 2)
+#         self.bn2 = nn.BatchNorm2d(512)  # BatchNorm for final layer
+
+#         self.fc = nn.Linear(512, num_classes)
+
+#     def repeat_block(self, in_channel, out_channel, num_blocks, stride):
+#         """Create repeated blocks for each layer."""
+#         bundle = []
+#         bundle.append(ResBlock(in_channel, out_channel, stride))  # First block with stride
+#         for _ in range(1, num_blocks):
+#             bundle.append(ResBlock(out_channel, out_channel, 1))  # Subsequent blocks with stride=1
+#         return nn.Sequential(*bundle)
+
+#     def forward(self, x):
+#         x = self.conv1(x)        # Initial Convolution
+#         x = self.bn1(x)          # BatchNorm after Conv
+#         x = F.relu(x)            # ReLU last
+
+#         x = self.bundle1(x)      # Layer 2~5
+#         x = self.bundle2(x)      # Layer 6~9
+#         x = self.bundle3(x)      # Layer 10~15
+#         x = self.bundle4(x)      # Layer 16~18
+#         x = self.bn2(x)          # BatchNorm for penultimate layer
+#         x = F.relu(x)            # ReLU after BatchNorm
+
+#         x = self.avgpool(x)      # Adaptive average pooling
+#         x = torch.flatten(x, 1)  # Flatten (batch x 512)
+#         x = self.fc(x)           # Fully connected layer
+#         return x
+
+# resnet-18 post-activation
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -68,8 +143,7 @@ class ResNet(nn.Module):
         x = self.fc(x)           # Fully connected layer
         return x
 
-
-
+# # resnet-18 pre-activation
 # import torch
 # import torch.nn as nn
 # import torch.nn.functional as F
