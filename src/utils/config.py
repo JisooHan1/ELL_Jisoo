d084a70fedd1e36922ee8dad5291eac27098b709
Jisoo <jshan3328@naver.com>
2025-02-03 01:10:35 +0900

    yaml & resnet

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,7 +1,7 @@
 import yaml
 
 # import yaml file
-def load_config(config_path="config.yaml"):
+def load_config(config_path="src/utils/config.yaml"):
     with open(config_path, "r") as file:
         return yaml.safe_load(file)
 
@@ -9,6 +9,11 @@ config = load_config()
 
 print(f"Model: {config['general']['model']}")
 print(f"Dataset: {config['general']['dataset']}")
-print(f"Epochs: {config['general']['epoch']}")
+print(f"Augment: {config['general']['augment']}")
+print(f"Epoch: {config['general']['epoch']}")
 print(f"Batch Size: {config['general']['batch_size']}")
+print(f"Variant: {config['train']['variant']}")
+print(f"ID Dataset: {config['train']['id_dataset']}")
+print(f"OE Dataset: {config['train']['oe_dataset']}")
+print(f"OOD Dataset: {config['train']['ood_dataset']}")
 print(f"OOD Method: {config['train']['method']}")
